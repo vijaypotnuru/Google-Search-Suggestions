ef7071b6db997a86b127a19f62001b7c63645e28
vijay potnuru <vijaypotnuru123@gmail.com>
2023-03-04 10:48:54 +0000

    Google Search Suggestions

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionItem, selectedInput} = props
+  const {suggestion} = suggestionItem
+  const onSelectedInput = () => {
+    selectedInput(suggestion)
+  }
+
+  return (
+    <li className="list-container" onClick={onSelectedInput}>
+      <p className="suggestion">{suggestion}</p>
+      <img
+        className="arrow-image"
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+      />
+    </li>
+  )
+}
+
+export default SuggestionItem
